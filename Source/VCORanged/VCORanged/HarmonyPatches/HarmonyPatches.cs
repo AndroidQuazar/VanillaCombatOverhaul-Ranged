031dc774733e67eaa4c2e4058337bf89179b2c44
XeoNovaDan <26175895+XeoNovaDan@users.noreply.github.com>
2020-03-05 00:20:02 +0000

    Initial project setup

---
+++ Source/VCORanged/VCORanged/HarmonyPatches/HarmonyPatches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VCORanged
+{
+
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            VCORanged.harmonyInstance.PatchAll();
+        }
+
+    }
+
+}
---
+++ Source/VCORanged/VCORanged/VCORanged.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VCORanged
+{
+
+    public class VCORanged : Mod
+    {
+
+        public VCORanged(ModContentPack content) : base(content)
+        {
+            harmonyInstance = new Harmony("OskarPotocki.VanillaCombatOverhaul.RangedModule");
+        }
+
+        public static Harmony harmonyInstance;
+
+    }
+
+}
