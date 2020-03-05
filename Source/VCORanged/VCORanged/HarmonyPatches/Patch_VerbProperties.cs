749072de4f4ffc8bb79f8e3c0d9723f6fc42a6ca
XeoNovaDan <26175895+XeoNovaDan@users.noreply.github.com>
2020-03-05 07:14:44 +0000

    Weapon accuracy

---
+++ Source/VCORanged/VCORanged/HarmonyPatches/Patch_VerbProperties.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VCORanged
+{
+
+    [StaticConstructorOnStartup]
+    public static class Patch_VerbProperties
+    {
+
+        public static class GetHitChanceFactor
+        {
+
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var instructionList = instructions.ToList();
+
+                var lastValueLoadInstruction = instructionList.Last(i => i.opcode == OpCodes.Ldloc_0);
+
+                for (int i = 0; i < instructionList.Count; i++)
+                {
+                    var instruction = instructionList[i];
+
+                    yield return instruction;
+
+                    // Look for the last instruction that loads 'value' local variable and remove all instructions ahead except the last one
+                    if (instruction == lastValueLoadInstruction)
+                    {
+                        while (true)
+                        {
+                            int j = i + 1;
+                            if (j == instructionList.Count)
+                                break;
+                            instructionList.RemoveAt(j);
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
